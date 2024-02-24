edb6fa284de015bdaff8b7f8d5a86430f6b295ba
ssteinmetz <samhsteinmetz@gmail.com>
2024-02-24 17:02:23 -0500

    fixed the database so that its fully connected through python

--- python/back.py
+++ python/back.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://samhsteinmetz:xRkRH6K465V74b0V@cluster0.tb0cj6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://samhsteinmetz:xRkRH6K465V74b0V@cluster0.tb0cj6m.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
@@ -16,12 +16,17 @@ except Exception as e:
 
 # Create a new database
     
-db = client["crowd_count"]
+db = client["people_count"]
+
 print("Database created!")
+
+print(db.list_collection_names())
 
 # Create a new collection
 collection = db["people_count"]
 print("Collection created!")
+
+print(db.list_collection_names())
 
 # Insert a new document
 document = {"people_count": 10}
@@ -33,5 +38,6 @@ query = collection.find({})
 print("Query results:")
 for x in query:
     print(x)
+
 
 
